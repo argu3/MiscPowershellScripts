c713dac375228263227cd0877343a043ebad86fe
Andrew Guthrie <93886464+argu3@users.noreply.github.com>
2024-07-19 20:11:43 -0400

    New scripts

---
+++ Get-AGCommandletWrapper.ps1
@@ -0,0 +1,52 @@
+﻿param(
+    $cmdletName,
+    [switch]$toFile,
+    [switch]$filterNullParams,
+    $cmdletNameModifierString = "Custom"
+)
+$helpInfo = get-help $cmdletName -Detailed
+if($helpInfo.count -gt 1)
+{
+    write-host "Multiple results returned" -BackgroundColor Red
+    break
+}
+#else
+$paramNames = $helpInfo.parameters.parameter.name
+$paramBlock = @("param(")
+$paramBlock += $paramNames.ForEach({"`t$" + $_ + ","})
+$paramBlock[$paramBlock.Count - 1] = $paramBlock[$paramBlock.Count - 1].Replace(",","")
+$paramBlock += ")"
+if($filterNullParams)
+{
+    $paramBlock += "`$params = @{}
+foreach(`$key in `$PSBoundParameters.keys)
+{
+    if(`$PSBoundParameters[`$key] -ne `$null)
+    {
+        `$params[`$key] = `$PSBoundParameters[`$key]
+    }
+}
+if(`$params.count -eq 0)
+{
+    `$output = $cmdletName @params
+}
+else
+{
+    `$output = $cmdletName
+}
+"
+}
+else
+{
+    $paramBlock += "`$output=@PSBoundParameters"
+}
+if($toFile)
+{
+    $cmdletNameModifierString = "-" + $cmdletNameModifierString
+    $fileName = $cmdletName.Replace("-",$cmdletNameModifierString) + ".ps1"
+    $paramBlock | Out-File $fileName
+}
+else
+{
+    $paramBlock
+}
---
+++ Get-AGWinEvent.ps1
@@ -0,0 +1,72 @@
+﻿param(
+    #Get-Winevent parameters
+    $ComputerName,
+    $Credential,
+    $FilterHashtable,
+    $FilterXml,
+    $FilterXPath,
+    $Force,
+    $ListLog,
+    $ListProvider,
+    $LogName,
+    $MaxEvents,
+    $Oldest,
+    $Path,
+    $ProviderName,
+
+    #Other parameter
+    [switch]$asPair
+    <#
+    if enabled:
+        returns a list of hash tables where the Event = the event object as Get-WinEvent returns it and the EventData = the event data
+    if disabled:
+        returns a pscustomobject with all of the event properties as well as an "EventData" property with the EventData
+    #>
+    #EventData is a hashtable where the key is the name and the value is the text value
+)
+
+#check for valid parameters
+$params = @{}
+foreach($key in $PSBoundParameters.keys)
+{
+    if($PSBoundParameters[$key] -ne $null -AND $key -ne "asPair")
+    {
+        $params[$key] = $PSBoundParameters[$key]
+    }
+}
+if($params.count -ne 0)
+{
+    $eventList = Get-WinEvent @params
+}
+
+#process EventData
+$newEventList = @()
+foreach($e in $eventList)
+{
+    if($asPair)
+    {
+        $pair = @{
+            EventData = ''
+            Event = ''
+        }
+        $eventData = @{}
+        ([xml]$e.ToXml()).Event.EventData.Data.ForEach{$eventData[$_.Name] = $_.'#text'}
+        $pair.EventData = New-Object pscustomobject -Property $eventData
+        $pair.Event = $e
+        $newEventList += $pair
+        }
+    else
+    {
+        #event properties to custom object
+        $extendedProperties = @{}
+        $e.psobject.Properties.Foreach{$extendedProperties[$_.Name] = $_.Value}
+        #event data properties to custom object
+        $eventData = @{}
+        ([xml]$e.ToXml()).Event.EventData.Data.ForEach{$eventData[$_.Name] = $_.'#text'}
+        $extendedProperties.EventData = New-Object pscustomobject -Property $eventData
+
+        $newEvent = New-Object pscustomobject -Property $extendedProperties
+        $newEventList += $newEvent
+    }
+}
+return $newEventList
